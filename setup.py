aeccfc66ed104d352d0341674ca88f9709775ef1
sai_bharath_chandra gutha <guthasaibharath06@gmail.com>
2021-08-13 11:48:40 +0530

    new commit setup daemon

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 import sys
+print("setup daemon begins")
+print("enter new login info")
 print("setup_started")
 print("setup_successful")
+print("setup daemon running background")
+print("setup dameon permission automatically granted")
